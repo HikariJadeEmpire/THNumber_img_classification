c5b631c5808ffbfcfed9a9ecf8369aa070dd777c
Hikari <118663358+HikariJadeEmpire@users.noreply.github.com>
2023-06-06 07:31:48 +0700

    update

--- apps/index00.py
+++ apps/index00.py
@@ -1,21 +1,11 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
+from dash import Dash, html
+from components.layout import create_layout
 
-from dash import Dash, html, dcc
-import plotly.express as px
-import pandas as pd
-
-app = Dash(__name__)
-
-colors = { 'background': '#111111', 'text': '#7FDBFF' }
-
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-
-app.layout = html.Div( children=[ html.H1( children='Hello Dash', style={ 'textAlign': 'center','color': colors['text'] } ) ,
-
-            html.Div(children='Dash: A web application framework for your data.', style={'textAlign': 'center', 'color': colors['text'] } )]
-            )
+def main() -> None:
+    app = Dash()
+    app.title = 'Number Classifier'
+    app.layout = create_layout
+    app.run()
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    main()
